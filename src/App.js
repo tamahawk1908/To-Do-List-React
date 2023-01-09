208632ea3ce5aba79d2970efd36deeeca6096c00
Michał Tamaka <tamahawk@wp.pl>
2023-01-09 20:54:00 +0100

    adding newTask interaction

--- src/App.js
+++ src/App.js
@@ -38,13 +38,24 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <><Header
       title="Lista zadań"
     />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__input" name="name" placeholder="Co jest do zrobienia?" autoFocus />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+
+const Form = ({addNewTask}) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit} >
+            <input
+                value={newTaskContent}
+                onChange={({target}) => setNewTaskContent(target.value)}
+                className="form__input"
+                name="name"
+                placeholder="Co jest do zrobienia?"
+                autoFocus />
+            <button
+                className="form__button">
+                Dodaj zadanie
+            </button>
+        </form >
+    )
+};
+
 
 export default Form;
