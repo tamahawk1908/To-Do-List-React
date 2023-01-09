ea298914d82600db1bd768329cc22948659ec65e
Michał Tamaka <tamahawk@wp.pl>
2023-01-09 20:11:08 +0100

    toggle HideDone interaction

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "zjeść obiad", done: true },
   { id: 2, content: "wypić piwo", done: false },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
   return (
     <><Header
       title="Lista zadań"
-      />
+    />
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
@@ -26,7 +31,11 @@ function App() {
           <Tasks tasks={tasks} hideDone={hideDone} />
         }
         additionalButtons={
-          <Buttons tasks={tasks} hideDone={hideDone} />
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
         }
       />
     </>
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,11 @@
 import "./style.css"
 
-const Buttons = ({ tasks, hideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button 
+                <button
+                    onClick={toggleHideDone}
                     className="buttons__button">
                     {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </button>
