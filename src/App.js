a24d728a5491468873487bef1f80380048ace667
Michał Tamaka <tamahawk@wp.pl>
2023-01-09 21:26:11 +0100

    added empty content wont submit

--- src/App.js
+++ src/App.js
@@ -39,14 +39,16 @@ function App() {
   };
 
   const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
+    if (content !== "") {
+      setTasks(tasks => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        },
+      ]);
+    }
   };
 
   return (
